81cf16cdf4249f026acbc2abdb41e4b6bd22fe99
MARIAGE Clémentine <clementine.mariage@gmail.com>
2026-09-03 14:51:15 +0200

    Début de rédaction de Die et fichier test.

--- boggle/src/main/java/org/example/Die.java
+++ boggle/src/main/java/org/example/Die.java
@@ -1,7 +1,22 @@
 package org.example;
 
-public class Die {
-    public Die(){
+import java.util.List;
 
+public class Die {
+
+    private String[] faces;
+
+    /**
+     * Constructor. Takes the List given in parameter and sets the attribute "faces" with the values.
+     * @param listFaces the list of the Die's faces
+     */
+    public Die(List<String> listFaces){
+
+        for(int i= 0; i<5;i++){
+            if(this.faces[i] == null)
+                throw new NullPointerException("Error while initializing a die!");
+
+            this.faces[i] = listFaces.get(i);
+        }
     }
 }
--- boggle/src/main/java/org/example/GameManager.java
+++ boggle/src/main/java/org/example/GameManager.java
@@ -1,29 +1,7 @@
 package org.example;
 
-import java.util.List;
 
 public class GameManager {
 
-    /**
-     *  Lists all the faces (interior List) for each dice (exterior List).
-     *  The same dices are always used, just randomly positioned, thus the choice of a constant.
-     */
-    public static final List<List<String>> DICE_FACES = List.of(
-            List.of("E","V","G","T","I","N"),
-            List.of("D","E","C","A","M","P"),
-            List.of("I","E","L","R","U","W"),
-            List.of("E","H","I","F","S","E"),
-            List.of("R","E","C","A","L","S"),
-            List.of("E","N","T","D","O","S"),
-            List.of("O","F","X","R","I","A"),
-            List.of("N","A","V","E","D","Z"),
-            List.of("E","I","O","A","T","A"),
-            List.of("G","L","E","N","Y","U"),
-            List.of("B","M","A","Q","J","O"),
-            List.of("T","L","I","B","R","A"),
-            List.of("S","P","U","L","T","E"),
-            List.of("A","I","M","S","O","R"),
-            List.of("E","N","H","R","I","S"),
-            List.of("E","T","U","K","N","O")
-    );
+
 }
