92090d7cb1e4188feef1493d34912a77cfef8667
MARIAGE Clémentine <clementine.mariage@gmail.com>
2026-09-09 01:53:17 +0200

    Création des tests de la classe Grid (jusque là réussis). reste à faire le shuffle.

--- boggle/src/test/java/c/mariage/GridTest.java
+++ boggle/src/test/java/c/mariage/GridTest.java
@@ -1,7 +1,62 @@
 package c.mariage;
 
+import c.mariage.exception.DieNotAddedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridTest {
 
+    private Grid grid;
+    private Die firstDie;
+    private Die lastDie;
+
+    @BeforeEach
+    public void before(){
+        this.grid = new Grid();
+        this.firstDie = new Die(this.grid.DICE_FACES.getFirst());
+        this.lastDie = new Die(this.grid.DICE_FACES.getLast());
+    }
+
+    @Test
+    public void attributesSizeOkTest(){
+        assertEquals(16, this.grid.getDiceArray().length);
+        assertEquals(4,this.grid.getSlots().length);
+        assertEquals(4,this.grid.getSlots()[3].length);
+    }
+
+    @Test
+    public void createDieTest(){
+        Die dieTest = this.grid.createDie(this.grid.DICE_FACES.getFirst());
+        assertEquals(this.firstDie.toString(), dieTest.toString());
+    }
+    @Test
+    public void diceAreCreatedCorrectly(){
+        //first die created
+        assertEquals (this.firstDie.toString(), this.grid.getDiceArray()[0].toString());
+        //last die created
+        assertEquals(this.lastDie.toString(),this.grid.getDiceArray()[15].toString());
+    }
+
+    @Test
+    public void slotsEmptyOnCreation(){
+        assertNull(this.grid.getSlots()[0][0]);
+        assertNull(this.grid.getSlots()[3][3]);
+    }
+
+    @Test
+    public void exceptionDieNotAddedTest(){
+        assertThrows(DieNotAddedException.class,()->this.grid.addDieToArray(0,this.firstDie));
+    }
+
+    @Test
+    public void setSlotTest(){
+        //placing in empty slot
+        assertEquals(0,this.grid.setSlot(0,0,this.firstDie));
+        //die really inserted
+        assertEquals(this.firstDie.toString(), this.grid.getSlots()[0][0].toString());
+        //trying a filled slot
+        assertEquals(1,this.grid.setSlot(0,0,this.firstDie));
+    }
 }
